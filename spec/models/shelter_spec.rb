f6e81de166a900bb86b1654730678a124b460d6a
foymikek <foymikek@gmail.com>
2025-04-09 12:02:01 -0600

    Add shelter model validation tests.

---
+++ spec/models/shelter_spec.rb
@@ -0,0 +1,10 @@
+require "rails_helper"
+
+describe Shelter, type: :model do
+  describe "validations" do
+    it {should validate_presence_of(:foster_program)}
+    it {should validate_presence_of(:rank)}
+    it {should validate_presence_of(:city)}
+    it {should validate_presence_of(:name)}
+  end
+end
