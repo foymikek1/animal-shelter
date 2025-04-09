6502524ba0912e14476886f61d62b36b80ca7b1b
foymikek <foymikek@gmail.com>
2025-04-09 15:55:01 -0600

    Add pet model validation and relationship test. Add pet migration and class. Tests passing.

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_04_09_180835) do
+ActiveRecord::Schema[7.1].define(version: 2025_04_09_215138) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "pets", force: :cascade do |t|
+    t.boolean "adoptable"
+    t.integer "age"
+    t.string "breed"
+    t.string "name"
+    t.bigint "shelter_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["shelter_id"], name: "index_pets_on_shelter_id"
+  end
 
   create_table "shelters", force: :cascade do |t|
     t.boolean "foster_program"
@@ -23,4 +34,5 @@ ActiveRecord::Schema[7.1].define(version: 2025_04_09_180835) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "pets", "shelters"
 end
