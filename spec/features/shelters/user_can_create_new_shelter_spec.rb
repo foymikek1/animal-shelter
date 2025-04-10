48ea9a3234679e03b9735c032a2f60249c0734f1
foymikek <foymikek@gmail.com>
2025-04-10 07:44:04 -0600

    Add create new shelter test spec.

---
+++ spec/features/shelters/user_can_create_new_shelter_spec.rb
@@ -0,0 +1,27 @@
+require "rails_helper"
+
+describe "user can create a shelter", type: :feature do
+    before(:each) do
+      @shelter = Shelter.create(foster_program: true, rank: 1, city: "Erie", name: "Erie Shelter")
+      @pet_1 = @shelter.pets.create(adoptable: true, age: 1, breed: "Collie", name: "Zues")
+      @pet_2 = @shelter.pets.create(adoptable: true, age: 2, breed: "Lab", name: "Horatio")
+      @shelter_1 = Shelter.create(foster_program: true, rank: 3, city: "Erie", name: "Erie Shelter")
+      @shelter_2 = Shelter.create(foster_program: true, rank: 2, city: "Boulder", name: "Boulder Shelter")
+  end
+  describe "they fill out a new shelter form" do
+    it "creates a new shelter and is displayed on shelters_path" do
+      visit "/shelters"
+      click_on "New Shelter"
+      expect(current_path).to eq("shelters/new")
+
+      fill_in "comment[foster_program]", with: true
+      fill_in "comment[rank]", with: 4
+      fill_in "comment[city]", with: "Longmont"
+      fill_in "comment[name]", with: "Longmont Shelter"
+      click_on "Create Shelter"
+
+      expect(current_path).to eq("/shelters")
+      expect(page).to have_content("Longmont Shelter")
+    end
+  end
+end
